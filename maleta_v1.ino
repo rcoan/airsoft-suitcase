10b9d09e3015be97e5a26de6b44156b3a3c6c7a9
rcoan <rvc.coan@gmail.com>
2023-08-29 14:43:13 -0300

    add led and comments to organize code sections

--- maleta_v1.ino
+++ maleta_v1.ino
@@ -27,14 +27,21 @@ Keypad keypad = Keypad(makeKeymap(NUMPAD_KEY_VALUES),
 // LCD display configs
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// PINOUT
+int siren_output = 13;
+int activity_led_output = 13;
 
 // application configs
 const char* INIT_STATE = "init_st";
 const char* CD_STATE = "cd_st";
+const char* PREMATURE_EXPLOSION_STATE = "pe_st";
+const char* EXPLOSION_STATE = "exploded_st";
 const char* ADM_STATE = "adm_st";
-
 char* state = INIT_STATE;
 
+int CD_TIME = 10;
+
+// password related
 #define Password_Length 7
 
 char MASTER_PASSWORD[Password_Length] = "000000";
@@ -43,22 +50,33 @@ char valid_passwords[3][Password_Length] = {
   "987654",
   "741852"
 };
-
 char cr_pass[Password_Length];
 char input_key;
 byte pass_count = 0;
-int attempts = 3;
-
+int attempts = 0;
+const int max_attempts = 3;
 char pass_team = 'N';
 
-
-
 void setup() {
-  // put your setup code here, to run once:
+  // serial for debug
   Serial.begin(9600);
+  
+  // LCD setup
   lcd.backlight();
   lcd.init();
+
+  // pinout setup
+  pinMode(siren_output, OUTPUT);
+  digitalWrite(siren_output, LOW);
+
+  // init variables
   reset_cr_pass_input();
+}
+// Utils
+void blink_light(int pin){
+  digitalWrite(pin, HIGH);
+  delay(50);
+  digitalWrite(pin, LOW);
 }
 
 void print_line(char* string, uint8_t line) {
@@ -71,6 +89,13 @@ void print_digit(char value, uint8_t line, uint8_t pos) {
   lcd.print(value);
 }
 
+// countdown
+
+void start_countdow(){
+
+}
+
+// Password functions
 void reset_cr_pass_input() {
   memset(cr_pass, '\0', Password_Length * sizeof(char));
   pass_count = 0;
@@ -78,39 +103,49 @@ void reset_cr_pass_input() {
 }
 
 void invalid_pass() {
+  // print error
   lcd.clear();
   print_line("Senha invalida!", 0);
   delay(2000);
   lcd.clear();
-  attempts--;
+  
+  // proccess error
+  attempts++;
+  
+  // blink number of attempts
+  for(int i = 0; i < max_attempts-attempts; i++){
+    blink_light(activity_led_output);
+    delay(100);
+  }
 }
 
 void validate_pass() {
-  Serial.println(cr_pass);
-  Serial.println(MASTER_PASSWORD);
-  Serial.println(cr_pass == MASTER_PASSWORD);
-  Serial.println(!strcmp(MASTER_PASSWORD, cr_pass));
   if (!strcmp(MASTER_PASSWORD, cr_pass)) {
     state = ADM_STATE;
   } else if (!strcmp(valid_passwords[0], cr_pass)) {
     state = CD_STATE;
     pass_team = 'A';
+    start_countdow();
   } else if (!strcmp(valid_passwords[1], cr_pass)) {
     state = CD_STATE;
     pass_team = 'B';
+    start_countdow();
   } else if (!strcmp(valid_passwords[2], cr_pass)) {
     state = CD_STATE;
     pass_team = 'C';
+    start_countdow();
   } else {
     invalid_pass();
   }
   reset_cr_pass_input();
 }
 
+// Proccess input functions
 void process_key_input() {
   input_key = keypad.getKey();
 
   if (input_key) {
+    blink_light(activity_led_output);
     switch (input_key) {
       case '#':
         validate_pass();
@@ -138,6 +173,8 @@ void process_key_input() {
   }
 }
 
+// journey orchestration functions
+
 void start_menu() {
   print_line("Insira a senha:", 0);
   process_key_input();
@@ -147,8 +184,10 @@ void start_menu() {
 void loop() {
   if (state == INIT_STATE) {
     start_menu();
+  } else if (state == CD_STATE){
+
   } else {
     print_line(state, 0);
-  print_digit(pass_team,1, 0);
+    print_digit(pass_team,1, 0);
   }
 }
